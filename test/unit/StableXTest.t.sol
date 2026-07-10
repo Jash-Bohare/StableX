6ee2fc121b8acdc56a7ce72af27050e6f67ebf27
Jash-Bohare <jashbohare@gmail.com>
2026-07-10 10:40:02 +0530

    Added unit tests

--- test/unit/StableXTest.t.sol
+++ test/unit/StableXTest.t.sol
@@ -14,6 +14,7 @@ contract StableXTest is Test{
     address public OWNER;
     address public USER1 = makeAddr("user1");
     address public USER2 = makeAddr("user2");
+    address public USER3 = makeAddr("user3");
     uint256 public constant STARTING_BALANCE = 100 ether;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -27,6 +28,7 @@ contract StableXTest is Test{
 
         vm.deal(USER1, STARTING_BALANCE);
         vm.deal(USER2, STARTING_BALANCE);
+        vm.deal(USER3, STARTING_BALANCE);
     }
 
     function testInitialSupplyMintedToOwner() public {
@@ -81,4 +83,183 @@ contract StableXTest is Test{
         vm.stopPrank();
     }
 
+    function testApproveAndTransferFrom() public {
+        vm.startPrank(OWNER);
+        stableX.transfer(USER1, 150);
+        vm.stopPrank();
+
+        vm.startPrank(USER1);
+        stableX.approve(USER2, 100);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        stableX.transferFrom(USER1, USER3, 10);
+        vm.stopPrank();
+
+        assertEq(stableX.allowance(USER1, USER2), 90);
+    }
+
+    function testInfiniteAllowanceNotReduced() public {
+        uint256 maxAllowance = type(uint256).max;
+        vm.startPrank(OWNER);
+        stableX.transfer(USER1, 150);
+        vm.stopPrank();
+
+        vm.startPrank(USER1);
+        stableX.approve(USER2, maxAllowance);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        stableX.transferFrom(USER1, USER3, 10);
+        vm.stopPrank();
+
+        assertEq(stableX.allowance(USER1, USER2), maxAllowance);
+    }
+
+    function testTransferFromRevertsIfBelowAllowance() public {
+        vm.startPrank(OWNER);
+        stableX.transfer(USER1, 150);
+        vm.stopPrank();
+
+        vm.startPrank(USER1);
+        stableX.approve(USER2, 100);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__InsufficientAllowance.selector, USER1, USER2, stableX.allowance(USER1, USER2), 150));
+        stableX.transferFrom(USER1, USER3, 150);
+        vm.stopPrank();
+    }
+
+    function testMintIncreasesSupply() public {
+        uint256 mintAmount = 500;
+        uint256 beforeMint = stableX.totalSupply();
+        vm.startPrank(OWNER);
+        stableX.mint(USER1, mintAmount);
+        uint256 afterMint = stableX.totalSupply();
+        vm.stopPrank();
+        assertEq(afterMint, beforeMint + mintAmount);
+    }
+
+    function testMintRevertsIfExceedsCap() public {
+        uint256 mintAmount = 600_000e18;
+        uint256 cap = stableX.maxSupply();
+        uint256 totalSupply = stableX.totalSupply();
+
+        vm.startPrank(OWNER);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__CapExceeded.selector, totalSupply, mintAmount, cap));
+        stableX.mint(USER1, mintAmount);
+        vm.stopPrank();
+    }
+
+    function testMintRevertsIfCallerNotMinter() public {
+        uint256 mintAmount = 500;
+
+        vm.startPrank(USER1);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__Unauthorized.selector, USER1));
+        stableX.mint(USER2, mintAmount);
+        vm.stopPrank();
+    }
+
+    function testBurnReducesSupply() public {
+        uint256 burnAmount = 500;
+        uint256 beforeBurn = stableX.totalSupply();
+        vm.startPrank(OWNER);
+        stableX.burn(burnAmount);
+        uint256 afterBurn = stableX.totalSupply();
+        vm.stopPrank();
+        assertEq(afterBurn, beforeBurn - burnAmount);
+    }
+
+    function testBurnRevertsIfInsufficientBalance() public {
+        uint256 burnAmount = 600_000e18;
+        uint256 balance = stableX.balanceOf(OWNER);
+
+        vm.startPrank(OWNER);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__InsufficientBalance.selector, OWNER, balance, burnAmount));
+        stableX.burn(burnAmount);
+        vm.stopPrank();
+    }
+
+    function testBlacklistedSenderCannotTransfer() public {
+        uint256 amount = 100;
+        vm.startPrank(OWNER);
+        stableX.transfer(USER1, 200);
+        stableX.blacklist(USER1);
+        vm.stopPrank();
+
+        vm.startPrank(USER1);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__Blacklisted.selector, USER1));
+        stableX.transfer(USER2, amount);
+        vm.stopPrank();
+    }
+
+    function testBlacklistedRecipientCannotReceive() public {
+        uint256 amount = 100;
+        vm.startPrank(OWNER);
+        stableX.transfer(USER1, 200);
+        stableX.blacklist(USER2);
+        vm.stopPrank();
+
+        vm.startPrank(USER1);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__Blacklisted.selector, USER2));
+        stableX.transfer(USER2, amount);
+        vm.stopPrank();
+    }
+
+    function testBlacklistedAddressCannotMintReceive() public {
+        uint256 mintAmount = 500;
+
+        vm.startPrank(OWNER);
+        stableX.blacklist(USER1);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__Blacklisted.selector, USER1));
+        stableX.mint(USER1, mintAmount);
+        vm.stopPrank();
+    }
+
+    function testRemoveFromBlacklistRestoresTransfer() public {
+        uint256 amount = 100;
+        uint256 initialBalance = stableX.balanceOf(USER2);
+
+        vm.startPrank(OWNER);
+        stableX.transfer(USER1, 200);
+        stableX.blacklist(USER1);
+        stableX.removeFromBlacklist(USER1);
+        vm.stopPrank();
+
+        vm.startPrank(USER1);
+        stableX.transfer(USER2, amount);
+        uint256 finalBalance = stableX.balanceOf(USER2);
+        vm.stopPrank();
+
+        assertEq(finalBalance, initialBalance + amount);
+    }
+
+    function testBlacklistRevertsIfCallerNotBlacklister() public {
+        vm.startPrank(USER1);
+        vm.expectRevert(abi.encodeWithSelector(StableX.StableX__Unauthorized.selector, USER1));
+        stableX.blacklist(USER2);
+        vm.stopPrank();
+    }
+
+    function testTransferOwnership() public {
+        vm.prank(OWNER);
+        stableX.transferOwnership(USER1);
+        assertEq(stableX.owner(), USER1);
+    }
+
+    function testRenounceOwnershipLocksAdmin() public {
+        vm.prank(OWNER);
+        stableX.renounceOwnership();
+        assertEq(stableX.owner(), address(0));
+    }
+
+    function testGrantAndRevokeRole() public {
+        vm.startPrank(OWNER);
+        stableX.grantMinter(USER1);
+        assertTrue(stableX.isMinter(USER1));
+        stableX.revokeMinter(USER1);
+        assertFalse(stableX.isMinter(USER1));
+        vm.stopPrank();
+    }
 }
