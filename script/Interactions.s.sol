54fd2630b88dfe684f3d9fc429e55460f06b6ec6
Jash-Bohare <jashbohare@gmail.com>
2026-07-11 19:19:42 +0530

    Added Interactions.s.sol

---
+++ script/Interactions.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {StableX} from "../src/StableX.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract GrantMinter is Script {
+    function run(address newMinter) external {
+        address mostRecentDeployment =
+            DevOpsTools.get_most_recent_deployment("StableX", block.chainid);
+
+        vm.startBroadcast();
+
+        StableX(mostRecentDeployment).grantMinter(newMinter);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract GrantBlacklister is Script {
+    function run(address newBlacklister) external {
+        address mostRecentDeployment =
+            DevOpsTools.get_most_recent_deployment("StableX", block.chainid);
+
+        vm.startBroadcast();
+
+        StableX(mostRecentDeployment).grantBlacklister(newBlacklister);
+
+        vm.stopBroadcast();
+    }
+}
